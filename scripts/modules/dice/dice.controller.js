5be376a06c1c51e34ab1317aa52b33c8c3dd88f4
Ted Bui <tedtbui@gmail.com>
2019-08-25 20:39:13 -0700

    ng-repeat die start

--- scripts/modules/dice/dice.controller.js
+++ scripts/modules/dice/dice.controller.js
@@ -11,6 +11,39 @@
 
     function diceController($scope) {
         $scope.isLoading = false;
+        $scope.attackDice = [
+            {
+                count: 0,
+                image: 'images/dices/blue/Blue_Attack_Dice_6.PNG'
+            },
+            {
+                count: 0,
+                image: 'images/dices/red/Red_Attack_Dice_6.PNG'
+            },
+            {
+                count: 0,
+                image: 'images/dices/yellow/Yellow_Attack_Dice_6.PNG'
+            },
+            {
+                count: 0,
+                image: 'images/dices/green/Green_Attack_Dice_6.PNG'
+            },
+        ];
+        $scope.defenseDice = [
+            {
+                count: 0,
+                image: 'images/dices/defense/grey/Grey_Defense_Dice_6.PNG'
+            },
+            {
+                count: 0,
+                image: 'images/dices/defense/black/Black_Defense_Dice_6.PNG'
+            },
+            {
+                count: 0,
+                image: 'images/dices/defense/brown/Brown_Defense_Dice_6.PNG'
+            },
+            
+        ];
 
         ///////////
 
