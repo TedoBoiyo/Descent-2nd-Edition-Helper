9833c2c066bde9df31fbf50a2b274840702bbeb3
Ted Bui <tedtbui@gmail.com>
2019-09-25 00:49:15 -0700

    CharacterSheet UI updates

--- scripts/modules/characterSheet/characterSheet.controller.js
+++ scripts/modules/characterSheet/characterSheet.controller.js
@@ -9,10 +9,22 @@
         '$scope',
     ];
 
-    function characterSheetController($scope) {
-
+    function characterSheetController($scope, $rootScope) {
+        $scope.showActiveSkills = false;
+        $scope.activeSkillFilter = activeSkillFilter;
+        $scope.changeFilter = changeFilter;
         ///////////
 
-       
+       function activeSkillFilter () {
+           if ($scope.showActiveSkills) {
+               return true;
+           } else {
+               return '';
+           }
+       }
+
+       function changeFilter() {
+           $scope.showActiveSkills = !$scope.showActiveSkills;
+       }
     }
 })();
